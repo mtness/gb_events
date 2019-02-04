97685a53c9d7fa5a907ed0f40b58e77fcc4efe29
Philipp Kitzberger <philipp@kitze.net>
2019-02-04 16:33:21 +0100

    TYPO3 8.7 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,8 +23,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '7.0.1',
     'constraints' => [
         'depends' => [
-            'php' => '5.5.0-7.0.99',
-            'typo3' => '6.2.0-7.6.99',
+            'php' => '5.5.0-7.2.99',
+            'typo3' => '6.2.0-8.7.99',
         ],
     ],
     'autoload' => [
