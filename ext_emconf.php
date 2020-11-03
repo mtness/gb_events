8fbf73fb2ab4b3645f2069f6339b2b74249ae834
Sebastian Stein <sebastian.stein@in2code.de>
2020-11-03 13:15:47 +0100

    [TASK] update ext_emconf dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,8 +23,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '7.0.1',
     'constraints' => [
         'depends' => [
-            'php' => '5.5.0-7.2.99',
-            'typo3' => '6.2.0-8.7.99',
+            'php' => '^7.0',
+            'typo3' => '8.7.0-9.5.99',
         ],
     ],
     'autoload' => [
