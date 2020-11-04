8efab23cdfd55b7e8f856f0e569e0e2559a473e4
Sebastian Stein <sebastian.stein@in2code.de>
2020-11-04 10:21:05 +0100

    [!!!][TASK] change vendor name from GuteBotschafter to In2code

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,7 +18,7 @@ $EM_CONF[$_EXTKEY] = [
     ],
     'autoload' => [
         'psr-4' => [
-            'GuteBotschafter\\GbEvents\\' => 'Classes',
+            'In2code\\GbEvents\\' => 'Classes',
         ],
     ]
 ];
