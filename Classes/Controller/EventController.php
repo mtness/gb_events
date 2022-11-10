715d36dac58e89d79f5babbef29b8aa8df80ed33
Alexander Kellner <alexander.kellner@in2code.de>
2022-11-10 17:20:25 +0100

    [FEATURE] Allow to filter list view for a special day
    with &tx_gbevents_main[filter][day]=2022-11-30

--- Classes/Controller/EventController.php
+++ Classes/Controller/EventController.php
@@ -6,32 +6,21 @@ use In2code\GbEvents\Domain\Model\Event;
 use Psr\Http\Message\ResponseInterface;
 use TYPO3\CMS\Extbase\Http\ForwardResponse;
 
-/**
- * Controller for the Event object
- */
 class EventController extends BaseController
 {
-    /**
-     * Displays all Events
-     */
-    public function listAction(): ResponseInterface
+    public function listAction(array $filter = []): ResponseInterface
     {
         switch ($this->settings['displayMode']) {
             case 'calendar':
                 return (new ForwardResponse('show'))->withControllerName('Calendar');
-
-                return $this->htmlResponse(null);
-                break;
             case 'archive':
                 return (new ForwardResponse('list'))->withControllerName('Archive');
-
-                return $this->htmlResponse(null);
-                break;
             default:
                 $events = $this->eventRepository->findAll(
                     $this->settings['years'],
                     (bool)$this->settings['showStartedEvents'],
-                    $this->settings['categories']
+                    $this->settings['categories'],
+                    $filter
                 );
                 $this->addCacheTags($events, 'tx_gbevents_domain_model_event');
                 $this->view->assign('events', $events);
@@ -39,12 +28,6 @@ class EventController extends BaseController
         return $this->htmlResponse();
     }
 
-    /**
-     * Displays a single Event
-     *
-     * @param Event $event
-     * @return void
-     */
     public function showAction(Event $event): ResponseInterface
     {
         $this->addCacheTags($event, 'tx_gbevents_domain_model_event');
