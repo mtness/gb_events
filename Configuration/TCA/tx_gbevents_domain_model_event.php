2c77a1077f0d14ecd95eabb207275432d29fdf48
mtness <markus@timtner.com>
2023-04-21 10:39:16 +0200

    [TASK] make description optional

--- Configuration/TCA/tx_gbevents_domain_model_event.php
+++ Configuration/TCA/tx_gbevents_domain_model_event.php
@@ -178,7 +178,7 @@ return [
                 'type' => 'text',
                 'cols' => 40,
                 'rows' => 15,
-                'eval' => 'trim,required',
+                'eval' => 'trim',
                 'enableRichtext' => true,
             ],
         ],
