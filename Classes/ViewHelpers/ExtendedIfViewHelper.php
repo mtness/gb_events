0c2667a284f08a17af3700c3558c07ffc9b7acdc
Markus Timtner <markus@timtner.com>
2023-10-13 16:15:43 +0200

    [TASK] added ViewHelpers

---
+++ Classes/ViewHelpers/ExtendedIfViewHelper.php
@@ -0,0 +1,42 @@
+<?php
+namespace In2code\GbEvents\ViewHelpers;
+
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractConditionViewHelper;
+
+/**
+ * Or Viewhelper
+ *
+ *
+ * Example
+ * ----------
+ * <vh:extendedIf condition="{logoIterator.isFirst}" or="{logoIterator.cycle} % 4">
+ *   <f:then>Do something</f:then>
+ *   <f:else>Do something else</f:else>
+ * </vh:extendedIf>
+ *
+ */
+
+class ExtendedIfViewHelper extends AbstractConditionViewHelper {
+
+    /**
+     * As this ViewHelper renders HTML, the output must not be escaped.
+     *
+     * @var bool
+     */
+    protected $escapeOutput = false;
+
+    /**
+     * renders <f:then> child if $condition or $or is true, otherwise renders <f:else> child.
+     *
+     * @param boolean $condition View helper condition
+     * @param boolean $or View helper condition
+     * @return string the rendered string
+     */
+    public function render($condition, $or) {
+        if ($condition || $or) {
+            return $this->renderThenChild();
+        } else {
+            return $this->renderElseChild();
+        }
+    }
+}
